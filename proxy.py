4ef07d151213fc0df5bc0d9e944317a890ad38d7
Anthony Bashi <tony.bashi@whiteops.com>
2015-12-09 12:24:52 -0800

    Added gzip capabilities

--- proxy.py
+++ proxy.py
@@ -1,17 +1,30 @@
 from werkzeug.wrappers import Request, Response, Headers
 import requests
+import gzip
+import StringIO
+
+def compress(content):
+
+    filebuff = StringIO.StringIO()
+
+    with gzip.GzipFile(mode='wb', fileobj=filebuff) as zipfile:
+        zipfile.write(content)
+
+    return filebuff.getvalue()
+
 
 @Request.application
 def application(request):
 
     serverresponse = requests.request(request.method,request.url,headers = dict(request.headers.items()))
 
-    print len(serverresponse.content)
+    responsecontent = serverresponse.content
 
-    clientresponse = Response(serverresponse.content)
+    if serverresponse.headers.get("content-encoding") == "gzip":
+        responsecontent = compress(responsecontent)
+
+    clientresponse = Response(responsecontent)
     clientresponse.headers = Headers(serverresponse.headers.items())
-
-    print len(clientresponse.data)
 
     return clientresponse
 
